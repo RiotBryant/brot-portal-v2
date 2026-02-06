a0bb2d42a9217457ccee6d017df69b6984f4ae27
Riot Bryant <258329960+RiotBryant@users.noreply.github.com>
2026-02-06 18:22:46 -0500

    Create page.tsx

---
+++ app/lounge/page.tsx
@@ -0,0 +1,16 @@
+export default function Lounge() {
+  return (
+    <main style={{ padding: 48 }}>
+      <h1>Brother Lounge</h1>
+
+      <h3>Chill Room 1</h3>
+      <iframe src="https://meet.jit.si/chill-room-1" width="100%" height="400" />
+
+      <h3>Chill Room 2</h3>
+      <iframe src="https://meet.jit.si/chill-room-2" width="100%" height="400" />
+
+      <h3>broT Lounge</h3>
+      <iframe src="https://meet.jit.si/brot-lounge" width="100%" height="400" />
+    </main>
+  );
+}

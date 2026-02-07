d3b5cb13b63819d726875c8d573372f2334821d4
Riot Bryant <258329960+RiotBryant@users.noreply.github.com>
2026-02-07 14:21:39 -0500

    Create route.ts

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { createClient } from "@/lib/supabase/server";
+
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
+
+  if (code) {
+    const supabase = createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(new URL("/portal", req.url), 303);
+}

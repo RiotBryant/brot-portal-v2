b37d2a8a3ad44d5d2e0d846310be5882aa490e1e
Riot Bryant <258329960+RiotBryant@users.noreply.github.com>
2026-02-14 17:56:53 -0500

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,4 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/lib/supabase/client";
+const supabase = createClient();
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
